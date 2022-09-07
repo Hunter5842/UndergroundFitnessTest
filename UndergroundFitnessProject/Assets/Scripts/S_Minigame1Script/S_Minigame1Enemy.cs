35c81d9b073c44585961c169560955932bf91a38
Hunter5842 <112580169+Hunter5842@users.noreply.github.com>
2022-09-07 14:25:03 -0400

    Matteo Patch Notes
    Changed the spawning of the enemies to more closely fit the notes Ben made.

--- UndergroundFitnessProject/Assets/Scripts/S_Minigame1Script/S_Minigame1Enemy.cs
+++ UndergroundFitnessProject/Assets/Scripts/S_Minigame1Script/S_Minigame1Enemy.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class S_Minigame1Enemy : MonoBehaviour
 {
     private float movementSpeed = 3f;
+    private bool direction = true;
+
+    private float spawnVariation = 9;
     //Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(Random.Range(spawnVariation, -spawnVariation), transform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         //A simple script that moves enemies to the right byt the movement speed multiplied by Delta Time. 
-        transform.position += new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+        if (direction)
+        {
+            transform.position += new Vector3(0, movementSpeed * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    public void SetUp(bool vertical, float speed)
+    {
+        direction = vertical;
+        movementSpeed = speed;
     }
 }
--- UndergroundFitnessProject/Assets/Scripts/S_Minigame1Script/S_Minigame1Spawner.cs
+++ UndergroundFitnessProject/Assets/Scripts/S_Minigame1Script/S_Minigame1Spawner.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class S_Minigame1Spawner : MonoBehaviour
 {
     public float spawnRate = 1;
-    public GameObject spawnedEnemy;
+    public GameObject spawnedEnemy; //Select the enemy you want to spawn. Must have the S_Minigame1Enemy script. 
+
+    [Header("If set to true, the enemies will spawn from up to down or from down to up. If set to false, the enemies will move accross the screen from left to right or right to left.")]
+    public bool verticalSpawn;
+
+
+    [Header("Determines how fast the enemies move.")]
+    public float enemySpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(spawnRate, spawnedEnemy));
+        spawnRate = 1 / spawnRate; //Makes it so the larger the number, the faster it spawns. 
+        StartCoroutine(spawnEnemy(spawnRate, spawnedEnemy)); //Spawns the enemies
     }
 
     // Update is called once per frame
@@ -21,9 +29,10 @@ public class S_Minigame1Spawner : MonoBehaviour
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(interval); 
 
         GameObject newEnemy = Instantiate(enemy, transform.position, transform.rotation);
+        newEnemy.GetComponent<S_Minigame1Enemy>().SetUp(verticalSpawn, enemySpeed); //A function I made to pass on some variables into the newly spawned enemies.
         StartCoroutine(spawnEnemy(spawnRate, spawnedEnemy));
     }
 }

49bff4b0f1eed1f4b3f4622698431602daa06b16
Hunter5842 <112580169+Hunter5842@users.noreply.github.com>
2022-09-12 14:38:57 -0400

    Matteo Patch Notes
    Added a title screen block out
    
    Started making buttons

---
+++ UndergroundFitnessProject/Assets/Scripts/S_TitleScreenScript/S_PlayButtonScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S_PlayButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string levelName; 
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
